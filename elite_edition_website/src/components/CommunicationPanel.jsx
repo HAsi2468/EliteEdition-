7d1e1dad4a0c7309f6a27361a92c3293162a5642
HAsi2468 <harshitsidapara2468@gmail.com>
2026-09-15 17:32:06 +0530

    chore: deploy updates

--- elite_edition_website/src/components/CommunicationPanel.jsx
+++ elite_edition_website/src/components/CommunicationPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import { api, getBaseUrl } from '../services/api';
 import { io } from 'socket.io-client';
 import TaskManagerPanel from './TaskManagerPanel';
